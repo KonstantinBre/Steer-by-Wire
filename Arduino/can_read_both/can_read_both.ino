7954f209db90e0e3a6b04d253888373d91f99885
Konstantin Bre <konstantin@bredenfeld.de>
2024-01-31 13:17:56 +0800

    problem with counter

--- Arduino/can_read_both/can_read_both.ino
+++ Arduino/can_read_both/can_read_both.ino
@@ -7,9 +7,10 @@ int counter=0;
 int sensor_number;
 int currentIndex;
 
-const int buffer_length = 500;
+const int buffer_length = 150;
 const int columns =2;
 int buffer[columns][buffer_length];
+const int amount_counter = buffer_length*2;
 
 
 struct can_frame canMsg;
@@ -39,49 +40,76 @@ void loop() {
       float angle = binaryBytesToInt(highByte_string, lowByte_string);
 
 
-      if (isEven(counter))
-      {
-         //even
-         sensor_number = 0;
-         currentIndex = counter/2;
-         buffer[sensor_number][currentIndex] = angle;
-
-      } else
-      {
-         //odd
-         sensor_number = 1;
-         currentIndex = (counter-1)/2;
-         buffer[sensor_number][currentIndex] = angle;
-      }
+      
       
 
-      //print the angle in monitor 
-      Serial.print("  Angle ");
-      Serial.print(sensor_number);
-      Serial.print(" : ");
-      Serial.print(angle);
-      Serial.print("   ");
-      Serial.print(counter);
-      Serial.print("\n");
-
-      //send data to Matlab
-      //writeToMatlab(angle);
-
-
-      counter+=1;
-
-      if (counter > 1000)
+      if (counter < amount_counter)
       {
-         
-         for (size_t i = 0; i < 2; i++)
+         if (isEven(counter))
          {
-            for (size_t j = 0; j < 500; j++)
+            //even
+            sensor_number = 0;
+            currentIndex = counter/2;
+            buffer[sensor_number][currentIndex] = angle;
+
+         } else
+         {
+            //odd
+            sensor_number = 1;
+            currentIndex = (counter-1)/2;
+            buffer[sensor_number][currentIndex] = angle;
+         }
+
+         //print the angle in monitor 
+         Serial.print("  Angle ");
+         Serial.print(sensor_number);
+         Serial.print(" : ");
+         Serial.print(angle);
+         Serial.print("   ");
+         Serial.print(counter);
+         Serial.print("\n");
+
+         //send data to Matlab
+         //writeToMatlab(angle);
+      }
+      else{
+
+         // Serial.print("Counter above 1000, starting ");
+
+       
+         // Serial.print("First buffer in range ? True=1 False =0   :");
+         // Serial.print(check_all_numb_of_arr_in_range(buffer[0]));
+
+        
+         if (check_all_numb_of_arr_in_range(buffer[0]) == check_all_numb_of_arr_in_range(buffer[1]))
+         {
+            Serial.print("ERRRRRRRRRRRRRRRRROOOOOOOORRRRRRRRRR");
+         }
+         
+               
+         if (isEven(counter))
+         {
+            if (check_all_numb_of_arr_in_range(buffer[0])==true)
             {
-               Serial.print(buffer[i][j]);
+               Serial.print("Steering wheel sensor:  ");
+               Serial.print(angle);
+               Serial.print("\n");
             }
-            
-         }        
-      }  
+            else
+            {
+               if (check_all_numb_of_arr_in_range(buffer[0])==false)
+               {
+                  Serial.print("Steering wheel sensor:  ");
+                  Serial.print(angle);
+                  Serial.print("\n");
+               }
+            } 
+         }     
+         
+      }
+      
+      counter+=1;
+      
    }
 }
 
